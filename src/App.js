be4cf6eb05023f0453c0c30f7e8e11acedc8419f
rsbabcock <rachael.s.babcock@gmail.com>
2019-01-06 17:09:32 -0600

    set up initial repo and added logo files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import logo from './img/magnoliaBW.png'
 class App extends Component {
   render() {
     return (
